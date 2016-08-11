2850c6fb57a08b53493c505cec30976633247e3c
dionefc <dfceconello@gmail.com>
2016-08-11 19:58:50 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 # mapping host ip
 DOCKERHOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
-printf "$APACHE_MOD_PROXY" >> /usr/local/apache2/conf/extra/httpd-proxy.conf
+echo -e "$APACHE_MOD_PROXY" >> /usr/local/apache2/conf/extra/httpd-proxy.conf
 
 exec "$@"
