87443f06f40363d59ab9257d73cfe1cc8782461d
dionefc <dfceconello@gmail.com>
2016-09-14 10:50:06 -0300

    upgrade httpd version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+touch /usr/local/apache2/conf/extra/httpd-proxy.conf
+touch /usr/local/apache2/conf/extra/httpd-access-control.conf
+
+rm /usr/local/apache2/conf/extra/httpd-proxy.conf
+rm /usr/local/apache2/conf/extra/httpd-access-control.conf
+
 # mapping host ip
 DOCKERHOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
