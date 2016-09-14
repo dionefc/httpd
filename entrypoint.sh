423a9119ab4097e74c6d4c70d90cd1407a2e9373
dionefc <dfceconello@gmail.com>
2016-09-14 08:17:34 -0300

    adicionada funcionalidade de acesso de controle

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 DOCKERHOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
 echo -e "$APACHE_MOD_PROXY" >> /usr/local/apache2/conf/extra/httpd-proxy.conf
+echo -e "$APACHE_ACCESS_CONTROL" >> /usr/local/apache2/conf/extra/httpd-access-control.conf
 
 exec "$@"
