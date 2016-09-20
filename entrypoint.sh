b20806ffba553c4312d7c6313a4a03cdbe0e8312
dionefc <dfceconello@gmail.com>
2016-09-20 16:27:30 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-set -e
-
-touch /usr/local/apache2/conf/extra/httpd-proxy.conf
-touch /usr/local/apache2/conf/extra/httpd-access-control.conf
-
-rm /usr/local/apache2/conf/extra/httpd-proxy.conf
-rm /usr/local/apache2/conf/extra/httpd-access-control.conf
 
 # mapping host ip
 DOCKERHOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
-echo -e "$APACHE_MOD_PROXY" >> /usr/local/apache2/conf/extra/httpd-proxy.conf
-echo -e "$APACHE_ACCESS_CONTROL" >> /usr/local/apache2/conf/extra/httpd-access-control.conf
+echo "$APACHE_MOD_PROXY" > /usr/local/apache2/conf/extra/httpd-proxy.conf
+echo "$APACHE_ACCESS_CONTROL" > /usr/local/apache2/conf/extra/httpd-access-control.conf
 
 exec "$@"
